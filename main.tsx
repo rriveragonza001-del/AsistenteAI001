e6852a6c3b450c410921050a397110a707f9832c
rriveragonza001-del <rrivera.gonza001@gmail.com>
2026-02-02 21:13:25 -0600

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,3 +1,11 @@
-<main className="p-10">
-  {renderView()}
-</main>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
