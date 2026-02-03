d10b4949c0fd090333d90a9e7b7f75d6d8a9fc6a
rriveragonza001-del <rrivera.gonza001@gmail.com>
2026-02-02 21:00:39 -0600

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,15 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error('No se encontró el elemento #root');
-}
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<main className="p-10">
+  {renderView()}
+</main>
