5b8764646bc9601a8ddc1d88670d01a3fbd7320b
rriveragonza001-del <rrivera.gonza001@gmail.com>
2026-02-02 20:06:26 -0600

    Fix root element error message and import CSS

--- main.tsx
+++ main.tsx
@@ -1,15 +1,15 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+const root = document.getElementById('root');
+
+if (!root) {
+  throw new Error('No se encontró el elemento #root');
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
